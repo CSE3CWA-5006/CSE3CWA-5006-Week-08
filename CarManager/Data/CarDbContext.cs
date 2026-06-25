8091ca7e78cd9d5100fdfb5bf82c12c9b8a2a49e
Shuo Ding <S.Ding@latrobe.edu.au>
2026-06-25 10:08:38 +1000

    Delete CarManager directory

--- CarManager/Data/CarDbContext.cs
+++
@@ -1,9 +0,0 @@
-using CarManager.Models;
-using Microsoft.EntityFrameworkCore;
-
-namespace CarManager.Data;
-
-public class CarDbContext(DbContextOptions<CarDbContext> options) : DbContext(options)
-{
-    public DbSet<Car> Cars => Set<Car>();
-}
